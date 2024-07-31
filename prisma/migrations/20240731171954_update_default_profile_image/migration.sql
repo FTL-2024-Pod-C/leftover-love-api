08e67a19ea6f7e32527bc31722ffe634a1dcc011
juliachaidez3 <jmchaidez@cpp.edu>
2024-07-31 10:24:20 -0700

    updated schema.prisma to include default image for profile photo

---
+++ prisma/migrations/20240731171954_update_default_profile_image/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "FoodPantry" ALTER COLUMN "profile_photo" SET DEFAULT 'https://leftoverlove.s3.us-east-2.amazonaws.com/default_profile_photo.jpg';
+
+-- AlterTable
+ALTER TABLE "Restaurant" ALTER COLUMN "profile_photo" SET DEFAULT 'https://leftoverlove.s3.us-east-2.amazonaws.com/default_profile_photo.jpg';
